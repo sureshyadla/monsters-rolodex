9f42964cd3632cbbff43bed124b069bd55512b11
Suresh Yadla <suresh.yadla@unilogcorp.com>
2020-10-14 11:42:54 -0400

    Adding files to github

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const SearchBox = ({placeholder, handleChange}) => {
+    return (
+        <input className='search' type='search' placeholder={placeholder} onChange={handleChange}/>
+    );
+}
+
