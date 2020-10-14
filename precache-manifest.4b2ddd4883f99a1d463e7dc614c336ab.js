ef92d6bfb26898e1964ef45bcdf44aba5c0075d3
Suresh Yadla <suresh.yadla@unilogcorp.com>
2020-10-14 11:41:46 -0400

    Updates

---
+++ precache-manifest.4b2ddd4883f99a1d463e7dc614c336ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9343426cf27ae39611aa50338427231",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6c4f7db401507b95a213",
+    "url": "/monsters-rolodex/static/css/main.e77b35f9.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c4f7db401507b95a213",
+    "url": "/monsters-rolodex/static/js/main.ddafda44.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
